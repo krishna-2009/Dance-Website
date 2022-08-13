47784d3a53c539a4785d628da7282e574b97a2d5
Krishna <krishna.p5@ahduni.edu.in>
2022-08-13 12:35:55 +0530

    Adding dance website

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'))   // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug')   // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views'))     // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res) => {
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on ${port}`);
+})
